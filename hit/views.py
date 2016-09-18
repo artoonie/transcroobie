eb0e95557eea2ed03bf73a93cb96e6d8389d90b8
Armin Samii <armin@avametric.com>
2016-09-18 01:01:48 -0700

    Create a basic django framework

---
+++ hit/views.py
@@ -0,0 +1,32 @@
+import os
+from django.http import HttpResponse
+from django.template import loader
+
+if os.environ.get("I_AM_IN_DEV_ENV"):
+    AMAZON_HOST = "https://workersandbox.mturk.com/mturk/externalSubmit"
+else:
+    AMAZON_HOST = "https://www.mturk.com/mturk/externalSubmit"
+
+def index(request):
+    if request.GET.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE":
+        # worker hasn't accepted the HIT (task) yet
+        pass
+    else:
+        # worked accepted the task
+        pass
+
+    worker_id = request.GET.get("workerId", "")
+
+    render_data = {
+        "worker_id": request.GET.get("workerId", ""),
+        "assignment_id": request.GET.get("assignmentId", ""),
+        "amazon_host": AMAZON_HOST,
+        "hit_id": request.GET.get("hitId", ""),
+    }
+
+    template= loader.get_template('hit/client.html')
+    response = template.render(render_data, request)
+    # without this header, your iFrame will not render in Amazon
+
+    return HttpResponse(response, content_type='application/xhtml+xml')
+
