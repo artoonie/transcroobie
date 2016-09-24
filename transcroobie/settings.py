661f2d09a91e31e129d5aa7332a89c7ed8e29a26
Armin Samii <armin@avametric.com>
2016-09-23 19:19:26 -0700

    Change page title, change/hide secret key

--- transcroobie/settings.py
+++ transcroobie/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q!0g%4dfqar+y*bp8w%1*(bs7*hup26(kje7v$9cl20kg^km-4'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
