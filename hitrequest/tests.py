b83ce8731e4a94ee628f1dbbf571ba8347a162f3
Armin Samii <armin@avametric.com>
2017-01-03 23:40:40 -0800

    Create super-basic unit tests

--- hitrequest/tests.py
+++ hitrequest/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class HitRequestTest(TestCase):
+    def setUp(self):
+        User.objects.create_user('user', 'test@testmail.com', 'password')
+
+    def test_login(self):
+        self.client.login(username='user', password='password')
+        response = self.client.get(reverse('hitrequest:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'hitrequest/list.html')
+
+    def test_failed_login(self):
+        self.client.login(username='user', password='wrongpass')
+        response = self.client.get(reverse('hitrequest:index'))
+        self.assertEqual(response.status_code, 302)
